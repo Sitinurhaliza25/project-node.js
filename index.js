e0adf08b23cff6e38c544741dbb2e5f0187fb968
liza <sitinurhalizasitanggang2505@gmail.com>
2023-11-15 17:25:09 +0700

    nambahlogin

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import conn from "./database.js";
+import { addUser
+ } from "./router/regis.js";
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/api/daftar",addUser);
+app.listen(3000, () => {
+    console.log("The Server starts on port 3000");
+});
